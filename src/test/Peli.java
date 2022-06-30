f54f5f0ce3f6d66a2a617c92da4a13054ab60a81
P <psurrielm@gmail.com>
2022-06-30 19:43:22 +0200

    json

---
+++ src/test/Peli.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Peli {
+
+    String name;
+    int duration;
+    String description;
+    double valoracion;
+
+    Peli(String name, int dur, String des, double val){
+        this.name = name;
+        duration = dur;
+        description = des;
+        valoracion = val;
+    }
+
+}
